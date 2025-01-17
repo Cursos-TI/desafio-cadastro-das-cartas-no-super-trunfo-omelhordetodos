799d3d5f6ece1451fb7059b4bf06c180c9718e4c
omelhordetodos <ilo21danielzem@gmail.com>
2025-01-17 01:17:51 +0000

    apliquei todas as entradas e saidas desejadas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    //Define todas as variaveis
+    int area, populacao, numeroPontosTuristicos;
+    char codigo[6];
+    char nomeC[20];
+    float pib;
+    //Cadastra todas as informações importantes para as cartas.
+    printf ("Digite o codigo da carta \n");
+    scanf ("%s", &codigo);
+    printf ("Digite o nome da cidade \n");
+    scanf ("%s", &nomeC);
+    printf ("Digite a area em m² da cidade \n");
+    scanf ("%d", &area);
+    printf ("Digite a população \n");
+    scanf ("%d", &populacao);
+    printf ("Digite o pib da cidade \n");
+    scanf ("%f", &pib);
+    printf ("Digite o numero de pontos turisticos \n");
+    scanf ("%d", &numeroPontosTuristicos);
+
+    //como foi sugerido na aula resumi a parte que demonstra as informações e apliquei todas em uma só linha.
+    printf ("Carta: %s - Cidade: %s\nPopulação: %d - PIB: %f\nArea: %d - Numero de pontos turisticos: %d\n", codigo, nomeC, populacao, pib, area, numeroPontosTuristicos);
 
     return 0;
 }
